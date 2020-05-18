383e04a04a0efd52c175add9c17e1ff395c5ed5d
Madhumita99 <42217182+Madhumita99@users.noreply.github.com>
2020-05-18 20:49:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-(function() {
-    'use strict';
+(function () {
+'use strict';
 
-    angular.module('LunchBreak', [])
-        .controller('LunchBreakController', LunchBreakController);
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-    LunchBreakController.$inject = ['$scope'];
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.checkDishes = function () {
+    var num = countDishes($scope.dishes);
+    $scope.message = buildMessage(num);
+  };
 
-    function LunchBreakController($scope) {
-        $scope.dishes = '';
-        $scope.message = '';
-        $scope.checked = false;
-
-        $scope.breakLunch = function() {
-            if ($scope.dishes.trim().length === 0) {
-                $scope.empty = true;
-            } else {
-                $scope.checked = true;
-                $scope.empty = false;
-                var arrayDishes = $scope.dishes.split(',');
-                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
-                    return v.trim() !== '';
-                });
-
-                if (arrayDishesWithoutEmptys.length <= 3) {
-                    $scope.message = 'Enjoy!';
-                } else {
-                    $scope.message = 'Too much!';
-                }
-            }
-        };
+  function countDishes(dishes) {
+    var count = 0;
+    if (dishes) {
+      var array = dishes.split(',');
+      for (var idx in array) {
+        if (array[idx].trim().length != 0) {
+          count++;
+        }
+      }
     }
+    return count;
+  }
+
+  function buildMessage(num) {
+    if (num === 0) {
+      return 'Please enter data first';
+    }
+    else if (num <= 3) {
+      return 'Enjoy!';
+    } else {
+      return 'Too much!';
+    }
+  }
+}
+
 })();
